7f4a94522e4beb4f091b51f2999dafc8149ec6e2
Staff GS 29 <staffgs29@student.dbs.ie>
2017-06-29 11:11:16 +0100

    Add first version of spell checker.

---
+++ spellchecker/spellcheck.py
@@ -0,0 +1,18 @@
+class SpellChecker(object):
+    def __init__(self):
+        self.words = []
+
+    def load_words(self, file_name):
+        self.words = open(file_name).readlines()
+        self.words = map(lambda x: x.strip(), self.words)
+
+    def check_word(self, word):
+        return word.strip('.').lower() in self.words
+
+    def check_words(self, sentence):
+        words_to_check = sentence.split(' ')
+        for word in words_to_check:
+            if not self.check_word(word):
+                 print('Word is misspelt : ' + word)
+                 return False
+        return True
